9ca695e265d3357ab675d23433740cfa9b3fc91a
Dhruvipatel12 <dhruvi.kaneriya@sanskartechnolab.in>
2023-03-29 18:19:10 +0530

    Delete Doctype

--- indiamart_integration/indiamart_integration/doctype/indiamart_enquiry/indiamart_enquiry.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Dhruvi and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('IndiaMart Enquiry', {
-	// refresh: function(frm) {
-
-	// }
-});
